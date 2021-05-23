5150313a5c6af39b34eefa2dbbeac54ce00abd2f
Mehmet Turan <mehmet.72.turan@gmail.com>
2021-05-23 16:56:23 +0000

    setting up backed tf config

---
+++ gke-cluster/variables.tf
@@ -0,0 +1,27 @@
+variable "deployment_name" {
+  default = "example"
+}
+
+variable "google_project_id" {
+  default = "get-from-console"
+}
+
+variable "cluster_name" {
+  default = "example"
+}
+
+variable "google_region" {
+  default = "us-central1"
+}
+
+variable "cluster_node_count" {
+  default = "3"
+}
+
+variable "cluster_version" {
+  default = "1.15"
+}
+
+variable "google_credentials_json" {
+  default = "service-account.json"
+}
